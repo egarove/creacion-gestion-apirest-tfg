1ee66b70813ccf562f8b3833d0fed626393ae0a2
egarove <eloygarov@gmail.com>
2026-03-02 11:54:21 +0100

    añado apptheme vacio

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color _primaryColor = Color(0xFF6B7280);
+  static const Color _secondaryColor = Color(0xFF9CA3AF);
+  static const Color _backgroundColor = Color(0xFFF9FAFB);
+  static const Color _surfaceColor = Color(0xFFFFFFFF);
+  static const Color _errorColor = Color(0xFEF87171);
+
+  static ThemeData lightTheme() {
+    return ThemeData(
+      
+    );
+  }
+}
