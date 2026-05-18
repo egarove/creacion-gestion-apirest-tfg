15df6209563a79eb2f0859d2e7a376466ed94033
libertoga16 <libertoga16@gmail.com>
2026-05-18 11:10:10 +0200

    frontend: build produccion, fuentes en link tags

--- server/appWeb/vite.config.ts
+++ server/appWeb/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   server: {
     host: '0.0.0.0',
     port: 3000,
