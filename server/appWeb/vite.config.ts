2d3f96fe5da3130b306ccb26244a7af1fa20e5ef
cvl0703 <cvl0703@gmail.com>
2026-05-19 12:28:02 +0200

    proxy usando la api interna

--- server/appWeb/vite.config.ts
+++ server/appWeb/vite.config.ts
@@ -14,11 +14,11 @@ export default defineConfig({
     allowedHosts: true,
     proxy: {
       '/get-all-apis': {
-        target: 'http://tfg-dam.libertoguillen.com:8000',
+        target: 'http://api:8000',
         changeOrigin: true
       },
       '^/(?!src|node_modules|@vite|@react-refresh|assets|favicon.ico).*': {
-        target: 'http://tfg-dam.libertoguillen.com:8000',
+        target: 'http://api:8000',
         changeOrigin: true,
         bypass: function (req) {
           if (req.headers.accept?.includes('text/html')) {
